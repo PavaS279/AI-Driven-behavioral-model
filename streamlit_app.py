aa5e71914dbd352bc981afbc2a398ae77f20fd03
PavaS279 <118591603+PavaS279@users.noreply.github.com>
2025-06-02 10:06:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from snowflake.snowpark
 
+cnx = st.connections("snowflake")
+session = cnx.session()
+
+df = session.table("DF_MODEL_INPUT")
+st.write(df)
 st.title("Hello, World!")
 st.write("Welcome to your first Streamlit app.")
